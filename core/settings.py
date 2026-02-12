a790bb6a28d79eb21c70cfacb7ba9b7c94646324
lucachak <lucas.lucachak9@gmail.com>
2026-02-12 21:59:17 +0000

    Fix static files: Add WhiteNoise

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -103,3 +104,4 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
