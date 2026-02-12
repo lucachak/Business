f0b14a609f032df2f84c7b1a7ca608e6196d9fc3
lucachak <lucas.lucachak9@gmail.com>
2026-02-12 21:50:05 +0000

    Fix build script: ensure pip installed

--- build_files.sh
+++ build_files.sh
@@ -3,11 +3,11 @@
 echo "Building Project..."
 
 # 1. Garante que o pip está instalado
-python3.9 -m ensurepip --default-pip
+python3 -m ensurepip --default-pip
 
 # 2. Instala as dependências
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # 3. Coleta os estáticos
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
