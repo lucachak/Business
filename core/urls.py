1de461f008bfae4aacd94851630a09b7350ba44a
lucachak <lucas.lucachak9@gmail.com>
2026-02-13 20:01:08 +0000

    updating

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", v.index, name="index"),
     path("project/<int:project_id>/", v.project_detail, name="project_detail"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
